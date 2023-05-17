0d09e021412ddfeb09b1dcc6e4650289a493b5a4
yangweiqi <yangweiqi@wafersystems.com>
2023-05-17 15:39:16 +0800

    first commit

---
+++ .umirc.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "umi";
+
+export default defineConfig({
+  npmClient: 'yarn',
+  presets: [require.resolve('@umijs/preset-vue')],
+  qiankun: {
+    slave: {},
+  },
+});
